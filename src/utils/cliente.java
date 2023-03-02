9ac48c10ef917bd3a2c8ac02ac1b5fe4431d1803
ApacheD2301 <jdmurcia04@misena.edu.co>
2023-03-02 17:53:58 -0500

    sadas

---
+++ src/utils/cliente.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class cliente {
+    public String Nick_Cliente;
+    public String Numero_Cliente;
+    public String Deuda_Cliente;
+    public String Abono_Cliente;
+    public String Saldo_Cliente;
+    public String Fecha_Hora;
+    public String Estado_Cliente;
+}
---
+++ src/utils/mesa.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class mesa {
+    public String Nro_Mesa;
+    public String Cant_Sillas;
+
+    public mesa(String nro_Mesa, String cant_Sillas) {
+        Nro_Mesa = nro_Mesa;
+        Cant_Sillas = cant_Sillas;
+    }
+}
